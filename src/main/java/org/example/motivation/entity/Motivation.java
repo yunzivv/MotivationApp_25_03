0373f6db49c74d32511181bff6fdcb0a49aa95f8
yunzivv <yunzivv@gmail.com>
2025-03-30 23:30:51 +0900

    주석 설정

--- src/main/java/org/example/motivation/entity/Motivation.java
+++ src/main/java/org/example/motivation/entity/Motivation.java
@@ -9,6 +9,7 @@ public class Motivation {
     private String source;
     private String motivation;
 
+    // 생성자, DTO
     public Motivation(int no, String source, String motivation) {
         this.no = no;
         this.source = source;
@@ -25,12 +26,6 @@ public class Motivation {
 
     public String getMotivation(){
         return this.motivation;
-    }
-
-    // motivation을 삭제 했을 경우
-    // 나머지 motivation 번호를 어떻게 구성할 것인지 ??
-    public void setNo(int no){
-        this.no = no;
     }
 
     public void setSource(String source){
