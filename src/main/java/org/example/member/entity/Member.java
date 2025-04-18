4d72aac288cab7393c5918673a13590ce4ec8216
yunzivv <yunzivv@gmail.com>
2025-04-18 13:08:58 +0900

    member 필드 추가

--- src/main/java/org/example/member/entity/Member.java
+++ src/main/java/org/example/member/entity/Member.java
@@ -1,4 +1,15 @@
 package org.example.member.entity;
 
 public class Member {
+    public int no;
+    public String name;
+    public String loginId;
+    public String loginPw;
+
+    public Member(int no, String name, String loginId, String loginPw) {
+        this.no = no;
+        this.name = name;
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+    }
 }
