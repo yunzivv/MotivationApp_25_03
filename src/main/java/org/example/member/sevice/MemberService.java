81e40e6ca08e2342e45d63ff1c7b1675de53aa25
yunzivv <yunzivv@gmail.com>
2025-04-18 16:46:02 +0900

    memberService 구조

--- src/main/java/org/example/member/sevice/MemberService.java
+++ src/main/java/org/example/member/sevice/MemberService.java
@@ -1,4 +1,24 @@
 package org.example.member.sevice;
 
 public class MemberService {
+
+    public MemberService() {
+
+    }
+
+    public void join(String name, String loginId, String loginPw){
+
+    }
+
+    public void login(String loginId, String loginPw){
+
+    }
+
+    public void logout(){
+
+    }
+
+    public void withdraw(String loginId, String loginPw){
+
+    }
 }
